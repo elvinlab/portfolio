a855ef5f6b99d9cc0646060410ef8a63f8eb125a
elvinlab <elvinlab@outlook.com>
2024-04-23 13:23:22 -0600

    Project structure, start of basic modules, white and dark themes

---
+++ src/utils/NavItems.ts
@@ -0,0 +1,22 @@
+export const NavItems = [
+  {
+    title: "Experiencia",
+    label: "experiencia",
+    path: "/#experiencia",
+  },
+  {
+    title: "Proyectos",
+    label: "proyectos",
+    path: "/#proyectos",
+  },
+  {
+    title: "Sobre mí",
+    label: "sobre-mi",
+    path: "/#sobre-mi",
+  },
+  {
+    title: "Contacto",
+    label: "contacto",
+    path: "mailto:elvinlab@outlook.com",
+  },
+];
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				logoFontLight: '#ab6b5a',
+				logoFontDark: '#bec4e3',
+				textLight: '#231e2d'
+			}, fontFamily: {
+				fira: "'Fira Code Variable', monospace",
+				protest: "'Protest Revolution', system- ui"
+			}
+		}
+	},
+	plugins: [],
+}
