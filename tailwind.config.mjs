480475654e4aade5b51b575a03a260513a444b0b
elvinlab <elvinlab@outlook.com>
2024-05-02 20:28:53 -0600

    New carousel component, finished the projects section and settings in the UI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
 	darkMode: 'class',
 	theme: {
 		extend: {
@@ -14,5 +14,5 @@ export default {
 			}
 		}
 	},
-	plugins: [],
+	plugins: [require('flowbite/plugin')],
 }
