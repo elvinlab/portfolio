8df9ed4d325085d724045c8f4d2efc98d48c3950
Elvin González <60045534+elvinlab@users.noreply.github.com>
2026-02-23 20:09:06 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://elvinlab.com/',
+  site: 'https://elvinlab.dev/',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
